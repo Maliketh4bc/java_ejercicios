9e9c45e01c39960c5a86ad8d10c74dec1d9cada6
Maliketh4bc <marcoalvar1212@gmail.com>
2025-11-06 12:20:54 +0100

    38

---
+++ unidad_05/ejercicio_05_38/src/App.java
@@ -0,0 +1,39 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        int num = 0;
+        int numS = 0;
+        int numA = 0;
+
+        System.out.print("Introduce la altura de la X: ");
+        num = Integer.parseInt(System.console().readLine());
+
+        int num1 = (num/2)+1;
+        int num2 = num - num1;
+
+        for(int i=num1; i>0; i--){
+
+            numS = num1 - i;
+            numA = i*2 - 1;
+
+            for(int j=0; j<numS; j++) System.out.print(" ");
+
+            for(int j=0; j<numA; j++) System.out.printf("*");
+
+            System.out.println();
+        }
+
+        for(int i=1; i<=num2; i++){
+
+            numS = num2 - i;
+            numA = i*2 + 1;
+
+            for(int j=0; j<numS; j++) System.out.print(" ");
+
+            for(int j=0; j<numA; j++) System.out.printf("*");
+
+            System.out.println();
+        }
+
+    }
+}
