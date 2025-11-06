0f4ee40ee3a4d583ca0cb2d912bfbfb571b2c905
Maliketh4bc <marcoalvar1212@gmail.com>
2025-11-06 14:33:52 +0100

    fix

--- unidad_05/ejercicio_05_63/src/App.java
+++ unidad_05/ejercicio_05_63/src/App.java
@@ -5,11 +5,11 @@ public class App {  //SIX SEVEEEEEEEEN
 
         int max = 0;
         int min = 0;
-        
-        int numS1 = 0;
-        int numA1 = 0;
-        int numS2 = 0;
-        int numA2 = 0;
+
+        int numSmax = 0;
+        int numAmax = 0;
+        int numSmin = 0;
+        int numAmin = 0;
 
         System.out.print("Introduce el número de la primera pirámide: "); num1 = Integer.parseInt(System.console().readLine());
         System.out.print("Introduce la altura de la segunda pirámide: "); num2 = Integer.parseInt(System.console().readLine());
@@ -25,29 +25,58 @@ public class App {  //SIX SEVEEEEEEEEN
         int diff = max - min;
         int cont = 0;
         for(int i=1; i<=max; i++){
-            
-            if(i <= num1){
 
-                numS1 = max - i;
-                numA1 = i*2 -1;
+            numSmax = max - i;
+            numAmax = i*2 -1;
 
-                for(int j=0; j<numS1; j++) System.out.printf(" ");
+            if(i <= max && max==num1){
 
-                for(int j=0; j<numA1; j++) System.out.printf("*");
+                for(int j=0; j<numSmax; j++) System.out.printf(" ");
 
-                for(int j=0; j<numS1; j++) System.out.printf(" ");
+                for(int j=0; j<numAmax; j++) System.out.printf("*");
+
+                for(int j=0; j<numSmax; j++) System.out.printf(" ");
 
                 if(i == max) System.out.print(" ");
             }
 
-            if(i > diff){
+            if(i > diff && min==num2){
                 cont++;
-                numS2 = ((max-diff) - cont) + ((i == max)?0:1);
-                numA2 = cont*2 -1;
+                numSmin = ((max-diff) - cont) + ((i == max)?0:1);
+                numAmin = cont*2 -1;
 
-                for(int j=0; j<numS2; j++) System.out.printf(" ");
+                for(int j=0; j<numSmin; j++) System.out.printf(" ");
 
-                for(int j=0; j<numA2; j++) System.out.printf("*");
+                for(int j=0; j<numAmin; j++) System.out.printf("*");
+
+                for(int j=0; j<numSmax; j++) System.out.printf(" ");
+            }
+
+            if(i <= diff && min==num1){
+                for(int j=0; j<min*2; j++) System.out.print(" ");
+            }
+
+            if(i > diff && min==num1){
+                cont++;
+                numSmin = (max-diff) - cont;
+                numAmin = cont*2 -1;
+
+                for(int j=0; j<numSmin; j++) System.out.printf(" ");
+
+                for(int j=0; j<numAmin; j++) System.out.printf("*");
+
+                for(int j=0; j<numSmin+1; j++) System.out.printf(" ");
+            }
+
+            if(i <= max && max==num2){
+
+                for(int j=0; j<numSmax; j++) System.out.printf(" ");
+
+                for(int j=0; j<numAmax; j++) System.out.printf("*");
+
+                for(int j=0; j<numSmax; j++) System.out.printf(" ");
+
+                if(i == max) System.out.print(" ");
             }
         
             System.out.println();
